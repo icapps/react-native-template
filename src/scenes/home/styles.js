ac3ce0f2333e901ee7eca1bd62ac06557a6f8c31
Maarten Anckaert <maarten.anckaert@icapps.com>
2018-06-07 08:23:20 +0200

    Don't align the props here either

--- src/scenes/home/styles.js
+++ src/scenes/home/styles.js
@@ -1,35 +1,35 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container  : {
-    padding        : 32,
-    display        : 'flex',
-    flexDirection  : 'column',
-    justifyContent : 'space-between',
-    flex           : 1,
+  container: {
+    padding: 32,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    flex: 1,
   },
-  lead       : {
-    textAlign : 'center',
+  lead: {
+    textAlign: 'center',
   },
-  button     : {
-    marginTop       : 10,
-    backgroundColor : 'rgb(255,45,85)',
-    borderRadius    : 3,
-    padding         : 10,
+  button: {
+    marginTop: 10,
+    backgroundColor: 'rgb(255,45,85)',
+    borderRadius: 3,
+    padding: 10,
   },
-  buttonText : {
-    color     : 'white',
-    textAlign : 'center',
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
   },
-  token      : {
-    borderRadius    : 3,
-    marginTop       : 10,
-    backgroundColor : 'rgb(0,122,255)',
-    padding         : 10,
+  token: {
+    borderRadius: 3,
+    marginTop: 10,
+    backgroundColor: 'rgb(0,122,255)',
+    padding: 10,
   },
-  tokenText  : {
-    color     : 'white',
-    textAlign : 'center',
+  tokenText: {
+    color: 'white',
+    textAlign: 'center',
   },
 });
 
